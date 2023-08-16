9387e2c9fcf73487f54be42347612329ebcc4fd8
HemanthKumar M <m.hemanthkmr143@gmail.com>
2023-08-16 17:06:13 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-red-500">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
